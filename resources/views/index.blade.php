ff647b3eaae4d1b65cbcb30d112fd5e067eebff2
Adam Hembrough <adam@good2learn.com>
2020-08-21 08:44:10 +0100

    Move js and font to local, move css to separate stylesheet

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,65 +6,12 @@
 
         <title>Adventure</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.cdnfonts.com/css/zx81spectrum" rel="stylesheet">
+        <link href="/css/styles.css" rel="stylesheet" type="text/css" />
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #000;
-                color: darkcyan;
-                font-family: 'ZX-Spectrum', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            a {
-                font-size: 12pt;
-                text-decoration: none;
-                color: #00f !important;
-            }
-
-            a:hover{
-                color: #f00 !important;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
     </head>
+    
     <body>
-        <div class="flex-center position-ref full-height">
+        <div class="flex-center full-height position-ref">
             <div class="content">
                 <div class="title m-b-md">
                     College<br>
@@ -77,4 +24,5 @@
             </div>
         </div>
     </body>
+    
 </html>
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -6,101 +6,15 @@
 
         <title>Adventure</title>
 
-        <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+        <script src="/js/jquery.js"></script>
         <script>
             $(function(){
                 $('#command').focus();
             });
         </script>
 
-        <!-- Fonts -->
-        <link href="https://allfont.net/allfont.css?fonts=zx-spectrum" rel="stylesheet" type="text/css" />
+        <link href="/css/styles.css" rel="stylesheet" type="text/css" />
         
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: black;
-                color: white;
-                font-family: 'ZX Spectrum';
-                font-size: 1em;
-                font-weight: 200;
-                height: 98vh;
-                margin: 0;
-                overflow:hidden;
-            }
-
-            .output::-webkit-scrollbar {
-              width: 10px;
-            }
-
-            .output::-webkit-scrollbar-track {
-              background: darkblue;
-            }
-            
-            .output::-webkit-scrollbar-thumb {
-              background-color: blue;
-              border-radius: 4px;
-              border: 3px solid black;
-            }
-
-            .input, .output {
-                margin:10px;
-                border:6px double;
-                border-radius: 6px;
-            }
-
-            .output {
-                scrollbar-width: thin;
-                scrollbar-color: black darkblue;
-                height: calc(100vh - 130px);
-                width: calc(100vw - 82px);
-                border-color: darkblue;
-                color: #bbb;
-                overflow-y: scroll;
-                padding: 10px;
-            }
-
-            .input, .command, .command_label {
-                width: calc(100vw - 62px);
-                color: green;
-                font-family: 'ZX Spectrum';
-                display: inline-block;
-            }
-
-            .input {
-                border-color: darkcyan;
-                position: absolute;
-                bottom: 0;
-                height: 50px;
-            }
-
-            .command {
-                height: calc(100% - 4px);
-                width: calc(100% - 40px);
-                padding:2px;
-                border: none;
-                background-color: black;
-                font-size: 1em;
-            }
-
-            .command:focus {
-                outline: none;
-            }
-
-            .command_label {
-                width:0;
-                padding:0;
-                margin:0;
-                margin-left: 4px;
-                text-align: right;
-            }
-
-            .inverse{
-                background-color: blue;
-                padding: 2px 0 2px 4px;
-            }
-
-        </style>
     </head>
     <body>
         <div class='output'>
