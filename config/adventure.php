1a1a9b4fe6c04889e15d5c8fd25951eb73082c20
Adam Hembrough <adam.hembrough@l1.com>
2023-10-11 17:16:51 +0100

    Update default location config

--- config/adventure.php
+++ config/adventure.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'start_location_slug' => env('START_LOCATION_SLUG', 'entrance_foyer'),
+    'start_location_slug' => env('START_LOCATION_SLUG', null),
     'developer_mode' => env('DEVELOPER_MODE', 'false'),
 ];
